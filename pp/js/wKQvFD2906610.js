e1417ab9e9469a68e7ac869c19e756ae59a616fe
ogrupolod <ogrupolod@gmail.com>
2025-09-16 14:48:59 -0300

    Add files via upload

---
+++ pp/js/wKQvFD2906610.js
@@ -0,0 +1,50 @@
+
+      function atomiShowItems({items}) {
+      try {
+        items.forEach((item) => {
+          const hiddenItem = [...document.querySelectorAll(`#${item}`), ...document.querySelectorAll(`.${item}`)];
+          console.log("hiddenItem", hiddenItem)
+          if (hiddenItem?.length > 0) {
+            hiddenItem.forEach(item => item.classList.remove("atomicat-delay"));
+          }
+        })
+      } catch (error) {
+        console.log(error);
+      }
+      }
+    (function() {
+          try {
+              const clickeventList = [{"compKey":"2d520e3","misc":{"type":"button"}},{"compKey":"f59174d","misc":{"type":"button"}},{"compKey":"2efe8cb","misc":{"type":"text"}},{"compKey":"cd71613","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
+      (function() {
+        try {
+          setTimeout(() => {
+            let elementsWithDelayClass = document.querySelectorAll(".atomicat-delay");
+            if(elementsWithDelayClass){
+              elementsWithDelayClass.forEach(element => {
+                element.classList.remove("atomicat-delay");
+              });
+            }
+            
+          }, 1820000);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    
